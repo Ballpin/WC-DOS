adfa47da2a17c688dd16e36b6080b54b586c9e3d
Ivan Mauricio <ivan@it-amigo.se>
2020-06-22 09:22:05 +0200

    Added all dos colors.

--- config/tailwind/tailwind.config.js
+++ config/tailwind/tailwind.config.js
@@ -7,12 +7,29 @@ module.exports = {
     },
     extend: {
       colors: {
-        cyan: '#9cdbff',
-      },
-      margin: {
-        '96': '24rem',
-        '128': '32rem',
-      },
+        dos: {
+          black: '#000000',
+          'dark-red': '#800000',
+          'dark-green': '#008000',
+          olive: '#808000',
+          'dark-blue': '#000080',
+          'dark-magenta': '#800080',
+          'dark-cyan': '#008080',
+          'light-grey': '#c0c0c0',
+          'money-green': '#c0dcc0',
+          'sky-blue': '#a6caf0',
+          'cream': '#fffbf0',
+          'medium-grey': '#a0a0a4',
+          'dark-grey': '#808080',
+          red: '#f00f00',
+          green: '#00ff00',
+          yellow: '#ff0ff0',
+          blue: '#0000ff',
+          magenta: '#ff00ff',
+          cyan: '#00ffff',
+          white: '#ffffff'
+        }
+      }
     }
   }
 }
